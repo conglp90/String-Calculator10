ec28e61c7e273b4be6d0c173f7b8f15bd8cc2660
conglp90 <conglp90@gmail.com>
2013-06-10 14:07:20 +0700

    1.1: green

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,6 +6,9 @@ public class Calculator {
 	public static int add(String input) {
 		// TODO Auto-generated method stub
 		int kq=0;
+		if(input==null||input.isEmpty())
+			return kq;
+		kq=Integer.parseInt(input);
 		return kq;
 	}
 }
