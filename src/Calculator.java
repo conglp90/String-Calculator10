3df7b79e78b433898d6d8ff71b24858e13cf4838
conglp90 <conglp90@gmail.com>
2013-06-10 14:22:08 +0700

    1.2 green

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,7 +8,16 @@ public class Calculator {
 		int kq=0;
 		if(input==null||input.isEmpty())
 			return kq;
-		kq=toInt(input);
+		
+		int value1, value2;
+		Matcher matcher=Pattern.compile("-?[0-9]+").matcher(input);
+		matcher.find(); 
+		value1=Integer.parseInt(matcher.group(0));
+		if(matcher.find())
+			value2=Integer.parseInt(matcher.group(0));
+		else 
+			value2 =0;
+		kq=value1+value2;
 		return kq;
 	}
 
