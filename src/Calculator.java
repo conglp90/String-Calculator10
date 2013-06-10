fc5b9f5d4d96dcf5882771df95b6827b49b9daab
conglp90 <conglp90@gmail.com>
2013-06-10 13:39:32 +0700

    Step1: Red

---
+++ src/Calculator.java
@@ -0,0 +1,8 @@
+public class Calculator {
+
+	public static int add(Object object) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/CalculatorTest.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void testNull() {
+		assertTrue(Calculator.add(null) == 0);
+	}
+
+	@Test
+	public void testBlankValue() {
+		assertTrue(Calculator.add("") == 0);
+	}
+
+////	//
+	@Test
+	public void testWithOneValue() {
+		assertTrue(Calculator.add("1") == 1);
+		assertTrue(Calculator.add("111") == 111);
+	}
+	
+	@Test
+	public void testWithTwoValue() {
+		assertTrue(Calculator.add("1,2") == 1);
+	}
+}
