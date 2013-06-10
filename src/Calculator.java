f29d076d9858cb2a9c81aef2d86a309464197a27
conglp90 <conglp90@gmail.com>
2013-06-10 14:28:48 +0700

    1.2 clean

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -6,18 +7,16 @@ public class Calculator {
 	public static int add(String input) {
 		// TODO Auto-generated method stub
 		int kq=0;
+		ArrayList<Integer> valueList = new ArrayList<Integer>() ;
 		if(input==null||input.isEmpty())
 			return kq;
-		
-		int value1, value2;
 		Matcher matcher=Pattern.compile("-?[0-9]+").matcher(input);
-		matcher.find(); 
-		value1=Integer.parseInt(matcher.group(0));
-		if(matcher.find())
-			value2=Integer.parseInt(matcher.group(0));
-		else 
-			value2 =0;
-		kq=value1+value2;
+		while (matcher.find()) {
+			valueList.add(toInt(matcher.group(0)));
+		}
+		for (Integer integer : valueList) {
+			kq+=integer;
+		}
 		return kq;
 	}
 
