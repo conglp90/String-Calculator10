1afe48cd74ae576d888b2356c4703fc3e94c4912
conglp90 <conglp90@gmail.com>
2013-06-10 14:02:44 +0700

    1.1: red

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,28 +6,6 @@ public class Calculator {
 	public static int add(String input) {
 		// TODO Auto-generated method stub
 		int kq=0;
-		if(input==null||input.isEmpty())
-			return kq;
-		else {
-			if(input.contains(","))
-			{
-				Matcher matcher=Pattern.compile("-?[0-9]+").matcher(input);
-				matcher.find();
-				int valueOne=toInt(matcher.group(0));
-				matcher.find();
-				int valueTwo=toInt(matcher.group(0));
-				kq=valueOne+valueTwo;
-			}
-			else
-				kq=toInt(input);
-		}
-			
 		return kq;
 	}
-
-	private static int toInt(String input) {
-		return Integer.parseInt(input);
-	}
-
-	
 }
